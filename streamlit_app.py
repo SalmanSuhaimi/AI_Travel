95466f97b7081fcab106193a5a32283e740be6e7
Salman Suhaimi <148429853+SalmanSuhaimi@users.noreply.github.com>
2023-11-28 05:44:09 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -360,15 +360,8 @@ def homepage():
     #header
     with header:
        st.markdown(h1_style, unsafe_allow_html=True)
-       st.markdown(
-            f"""
-            <div style='text-align: center;'>
-                ![GIF]({github_gif_url})
-            </div>
-            """,
-            unsafe_allow_html=True
-            )
-	st.markdown("""<h1 class="app-title"><strong>TravelMate AI</strong></h1>""", unsafe_allow_html=True)
+       st.markdown(f"""<div style='text-align: center;'>![GIF]({github_gif_url})</div>""",unsafe_allow_html=True)
+	   st.markdown("""<h1 class="app-title"><strong>TravelMate AI</strong></h1>""", unsafe_allow_html=True)
     
     #subheader
     with description:
